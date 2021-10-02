45093d0d4d9162ea64f16fc34d8911b11b619a1c
Tingwen Guo <gtingwen@outlook.com>
2021-10-01 20:18:25 -0400

    working version of generate smv

--- slc/src/compound_rule_object.py
+++ slc/src/compound_rule_object.py
@@ -1,4 +1,5 @@
 import distutils.util
+import re
 
 
 class CompoundRule:
@@ -8,27 +9,74 @@ class CompoundRule:
     actions: a string representing actions
     other_compound_rules: list of names of all other compound rules
     """
-    def __init__(self, name, init_states, actions, other_compound_rules):
+
+    def __init__(self, name, init_states, actions, other_compound_rules, table_order):
         self.name = name
-        self.init_states = [distutils.util.strtobool(v) for v in init_states]
-        self.actions = actions
+        self.num_tables = len(str.split(name, "_"))
+        self.init_states = str.split(init_states, ",")
+        self.actions = str.split(actions, ",")
         self.other_compound_rules = other_compound_rules
+        self.table_order = table_order
 
     def to_model_string(self):
         res = ""
         res += "MODULE " + self.name + "("
-        for rule in self.other_compound_rules:
-            res += rule + ","
+        for i, rule in enumerate(self.other_compound_rules):
+            res += rule
+            if i != len(self.other_compound_rules) - 1:
+                res += ","
         res += ")\n"
 
         # Var section
         res += "VAR" + "\n"
-        res += "\t" + "active1:boolean;" + "\n"
-        res += "\t" + "active2:boolean;" + "\n"
+        # condition on all self variables
+        join_condition = ""
+        for i in range(self.num_tables):
+            res += "\t" + "active" + str(i + 1) + ":boolean;" + "\n"
+            join_condition += "self.active" + str(i + 1)
+            if i != self.num_tables - 1:
+                join_condition += " & "
 
         # Assign section
         res += "ASSIGN\n"
-        i = 0
-        for state in self.init_states:
-            res += "\tinit(" + "active" + str(i)
-            i += 1
+        # init
+        for i in range(len(self.init_states)):
+            res += "\tinit(" + "active" + str(i + 1) + ") := "
+            res += self.init_states[i].upper() + ";" + "\n"
+
+        # Next
+        for idx, action in enumerate(self.actions):
+            # search for tableName-ruleName
+            if not (action.startswith("delete") or action.startswith("add")): continue
+
+            target_table = re.search(r'\((.*?)\)', action).group(1).split("-")[0]
+            target_rule = re.search(r'\((.*?)\)', action).group(1).split("-")[1]
+            nth = self.table_order.index(target_table)
+
+            other_rules_to_change = list()
+            for r in self.other_compound_rules:
+                if str.split(r, "_")[nth] == target_rule:
+                    other_rules_to_change.append(r)
+
+            if str.split(self.name, "_")[nth] == target_rule:
+                res += "\tnext(" + "self.active" + str(nth + 1) + ") := case\n"
+                res += "\t\t" + join_condition + " : "
+                if action.startswith("delete"):
+                    res += "FALSE"
+                else:
+                    res += "TRUE"
+                res += ";\n"
+                res += "\t\tTRUE : " + "self.active" + str(nth + 1) + ";\n\t\tesac;\n"
+
+            # add next section for other rules
+            for r in other_rules_to_change:
+                res += "\tnext(" + r + ".active" + str(nth + 1) + ") := case\n"
+                res += "\t\t" + join_condition + " : "
+                if action.startswith("delete"):
+                    res += "FALSE"
+                else:
+                    res += "TRUE"
+                res += ";\n"
+                res += "\t\tTRUE : " + r + ".active" + str(nth + 1) + ";\n\t\tesac;\n"
+
+        return res
--- slc/src/generate_smv.py
+++ slc/src/generate_smv.py
@@ -13,8 +13,28 @@ def read_compound_table(f):
         name = key
         init_states = json_object[key]["active"]
         actions = json_object[key]["action"]
-        res.append(compound_rule_object.CompoundRule(key, init_states, actions, json_object.keys()))
+        other_compound_rules = list(json_object.keys())
+        other_compound_rules.remove(key)
+        res.append(compound_rule_object.CompoundRule(key, init_states, actions, other_compound_rules, ["fw", "IDPS"]))
+    for compoundRule in res:
+        print(compoundRule.to_model_string())
 
+    main_mod = "MODULE main" + "\n"
+
+
+    for key in json_object.keys():
+        main_mod += "\t" + key + " : process "
+        main_mod += key + "("
+
+        other_rules = list(json_object.keys())
+        other_rules.remove(key)
+        for i, rule in enumerate(other_rules):
+            main_mod += rule
+            if i != len(other_rules) - 1:
+               main_mod += ","
+
+        main_mod += ")\n"
+    print(main_mod)
 
 def main():
     parser = argparse.ArgumentParser(description='converts compound table to a NuSMV file')
